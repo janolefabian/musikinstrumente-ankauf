de480af14ea6de49873d3d8534423b8e6864892b
Jan <jean-eau-le@gmx.net>
2026-08-12 22:11:42 +0200

    Expand core SEO landing pages

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,12 @@
 export const site = {
   name: 'Musikinstrument Ankauf',
   domain: 'musikinstrument-ankauf.de',
-  email: 'kontakt@musikinstrument-ankauf.de'
+  url: 'https://musikinstrument-ankauf.de',
+  email: 'kontakt@musikinstrument-ankauf.de',
+  address: {
+    street: 'Utrechter Str. 34',
+    postalCode: '13347',
+    city: 'Berlin',
+    country: 'DE'
+  }
 };
